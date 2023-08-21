0f9be2c5f6b7809588d1184142a9a94eb17ca59e
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-08-21 12:34:42 +0300

    JSonBody middleware created

--- createCustomMiddleware/createCustomMiddleware/Middlewares/JSonBodyMiddleware.cs
+++ createCustomMiddleware/createCustomMiddleware/Middlewares/JSonBodyMiddleware.cs
@@ -2,5 +2,26 @@
 {
     public class JSonBodyMiddleware
     {
+        private readonly RequestDelegate _next;
+
+        public JSonBodyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //bu middleeare ne iş yapacaksa onu bu metot yapar ve ardından  sonraki middleware'a (_next) gönderir.
+            //amaç:
+            //gelen request POST ise ve gövdede JSON içeriyorsa; bu içeriği ayır.
+
+            if (context.Request.Method == "POST" && context.Request.ContentType.StartsWith("application/json"))
+            {
+
+            }
+
+
+            await _next(context);
+        }
     }
 }
