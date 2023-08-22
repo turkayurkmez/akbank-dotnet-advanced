caaeb9dd4ef588f0ffb1d8f43862683fdd1e233c
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-08-22 11:06:22 +0300

    Option Pattern for .net core

---
+++ OptionPattern/OptionPattern/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using OptionPattern.Models;
+using OptionPattern.Settings;
+using System.Diagnostics;
+
+namespace OptionPattern.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SmtpSettings _option;
+
+        public HomeController(ILogger<HomeController> logger, IOptions<SmtpSettings> options)
+        {
+            _logger = logger;
+            _option = options.Value;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View(_option);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OptionPattern/OptionPattern/Settings/SmtpSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace OptionPattern.Settings
+{
+    public class SmtpSettings
+    {
+        public string Host { get; set; }
+        public bool EnableSSL { get; set; }
+        public int Port { get; set; }
+
+        public DefaultValues Values { get; set; }
+
+    }
+
+    public class DefaultValues
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+    }
+}
